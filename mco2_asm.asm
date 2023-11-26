013a593126548a376f53d61d293fc40d51ca54e4
rpineda26 <91407317+rpineda26@users.noreply.github.com>
2023-11-27 04:58:22 +0800

    add: initial c aand asm code

---
+++ mco2_asm.asm
@@ -0,0 +1,69 @@
+global _imgAvgFilter
+segment .data
+image_size_x: dd 0
+image_size_y: dd 0
+sampling_window_size: dd 0
+row_index: dd 0
+col_index: dd 0
+segment .text
+_imgAvgFilter:
+	push ebp
+	mov ebp, esp
+	mov esi, [ebp+8]
+	mov edi, [ebp + 12]
+	mov dword[sampling_window_size], [ebp + 16]
+	mov dword[image_size_x], [ebp + 20]
+	mov dword[image_size_y], [ebp + 20]
+	
+	IMUL eax, ebx
+	mov ecx, eax
+	mov dword[row_index], 0x1; row index
+	mov dword[col_index], 0x1 ;col index
+L1:
+	call checkBorder ;only add apply filter to the pixels not found in the border
+	add dword[row_index], 0x1
+	add dword[col_index], 0x1
+	cmp edx, 0
+	jne averaged
+	add esi, 4
+	loop L1
+	xor eax, eax
+	ret
+checkBorder: ;if row index not equal 0 or image_size_X
+		;if col_index not equal 0 or image_size_y
+	mov eax, [row_index]
+	mov ebx, [image_size_x]
+	cpm eax, ebx
+	jge isBorder
+	cmp dword[row_index], 0x0
+	jge isBorder
+	mov eax, [col_index]
+	mov ebx, [image_size_y]
+	cmp eax, ebx
+	jge isBorder
+	cmp dword[col_index], 0x0
+	jge isBrder
+	mov esp, ebp
+	ret 
+isBorder:
+	mov edx, 1
+	mov esp, ebp
+	ret 
+average:
+	mov ebx, [sampling_window_size]
+	imul ebx, ebx
+	mov eax, [esi -4]
+	add eax, [esi]
+	add eax, [esi+4]
+	add eax, [esi +1 * ebx]
+	add eax, [esi + 1 * ebx + 4]
+	add eax, [esi + 1 * ebx - 4]
+	add eax, [esi - 1* ebx]
+	add eax, [esi - 1 * ebx + 4]
+	add eax, [esi -1 * ebx -4]
+
+	idiv ebx
+	mov [edi], eax
+	add edi, 4
+	add esi, 4
+	loop L1
---
+++ mco2_c.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+extern void vecadd(int n, int* arr1, int* arr2, int*arr3);
+extern void imgAvgFilter(int* input_image, int* filtered_image, int sampling_window_size, int image_size_x,int image_size_y);
+int main(){
+	int i, j, sampling_window_size, image_size_x, image_size_y;;
+	int* input_image;
+	int* filtered_image;
+	input_image = (*int)malloc(N*sizeof(*input_image));
+	filtered_image = (*int)malloc(N*sizeof(filtered_image));
+//get input for size of array
+	printf("Enter x number of rows: ");
+	scanf("%d", &image_size_x);
+	printf("Enter y number of columns: ");
+	scanf("%d", &image_size_y);
+	printf("Enter sampling window size: ");
+	scanf("%d", &sampling_window_size);
+//get image input
+	printf("Enter the pixel values for the input image: \n");	
+	for(i=0;i<image_size_x;i++){
+		for(j = 0; j< image_size_y; j++){
+			printf("pixel[%d][%d]: ",i,j);
+			scanf("%d", &input_image[i*image_size_x +j]);
+		}
+	}
+
+	imgAvgFilter(input_image,filtered_image,sampling_window_size,image_size_x, image_size_y);
+
+//display filtered image
+	for(i=0; i< image_size_x;i++){
+		for(j=0;j<image_size_y; j++){
+			printf("%d  ", filtered_image[i*image_size_x+image_size_y]);
+		}
+		printf("\n");
+	}
+	return 0;
+}
