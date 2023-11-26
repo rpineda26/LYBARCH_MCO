c4f3482f356b3c298823bca86c44fc60878a5b65
rpineda26 <91407317+rpineda26@users.noreply.github.com>
2023-11-27 06:12:26 +0800

    array allocation

--- mco2_c.c
+++ mco2_c.c
@@ -3,11 +3,10 @@
 extern void vecadd(int n, int* arr1, int* arr2, int*arr3);
 extern void imgAvgFilter(int* input_image, int* filtered_image, int sampling_window_size, int image_size_x,int image_size_y);
 int main(){
-	int i, j, sampling_window_size, image_size_x, image_size_y;;
+	int i, j,N, sampling_window_size, image_size_x, image_size_y;;
 	int* input_image;
 	int* filtered_image;
 	input_image = (int*)malloc(N*sizeof(*input_image));
-	filtered_image = (int*)malloc(N*sizeof(filtered_image));
 //get input for size of array
 	printf("Enter x number of rows: ");
 	scanf("%d", &image_size_x);
@@ -15,6 +14,10 @@ int main(){
 	scanf("%d", &image_size_y);
 	printf("Enter sampling window size: ");
 	scanf("%d", &sampling_window_size);
+	
+	N = image_size_x * image_size_y;
+	int input_image[N]= {0};
+	int filtered_image[N]= {0};
 //get image input
 	printf("Enter the pixel values for the input image: \n");	
 	for(i=0;i<image_size_x;i++){
