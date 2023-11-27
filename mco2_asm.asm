34ace54596e85b89ee02147afffc5bb13006ed8d
MrMoist0421 <chenijen298877@gmail.com>
2023-11-27 23:06:26 +0800

    352

--- mco2_asm.asm
+++ mco2_asm.asm
@@ -73,7 +73,7 @@ addRow:
 	mov edx, [col_index]
 	add  edx, ebx
 	mov [add_y], edx
-	cmp [add_x], 0
+	cmp dword[add_x], 0
 	jge  addCol
 	jmp divideNum 
 addCol:	
@@ -86,12 +86,12 @@ addCol:
 	add eax, [esi]
 	add esi, edx
 	
-	dec [add_y]
+	dec dword[add_y]
 	cmp dword[add_y], 0
 	jle backToRow
 	jmp addCol
 backToRow:
-	dec [add_x]
+	dec dword[add_x]
 	jmp addRow
 divideNum:
 	mov ebx, [sampling_window_size]
