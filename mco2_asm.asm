ee67167cc5a528821ec7f04d0e7d52d8bb4e0070
rpineda26 <91407317+rpineda26@users.noreply.github.com>
2023-11-28 14:54:23 +0800

    Revert "sdaf"
    This reverts commit 39765e43b5167513e4475cad18087068cfdda5ed.

--- mco2_asm.asm
+++ mco2_asm.asm
@@ -57,7 +57,6 @@ isBorder:
 	mov edx, 1
 	ret 
 average:
-	mov eax, 0
 	mov ecx, [sampling_window_size]
 	shr ecx, 1
 	mov ebx, ecx
@@ -73,14 +72,14 @@ average:
 addRow: 
 	mov ebx, [sampling_window_size]
 	shr ebx, 1
+	mov ecx, ebx
 	mov edx, [col_index]
 	sub  edx, ebx
 	mov [add_y], edx
 	mov edx, [row_index]
-	add edx, ebx
-	mov ebx, 0
-	cmp [add_x], edx
-	jle  addCol
+	sub edx, ebx
+	cmp dword[add_x], edx
+	jge  addCol
 	jmp divideNum 
 addCol:		
 	add eax, [esi]
@@ -89,10 +88,9 @@ addCol:
 	mov edx, [sampling_window_size]
 	shr edx, 1
 	mov ecx, [col_index]
-	add ecx, edx
-	add esi, 4
-	cmp [add_y], ecx
-	jg backToRow
+	sub ecx, edx
+	cmp dword[add_y], ecx
+	jle backToRow
 	jmp addCol
 backToRow:
 	inc dword[add_x]
